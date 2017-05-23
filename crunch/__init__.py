c1550ad2b63534cb5c4de33946039a1762bcd4ea
A̶̲̅ɴ̶̲̅ᴅ̶̲̅ʏ̶̲̅ ̶̲̅H̶̲̅ᴀ̶̲̅ᴡ̶̲̅ᴋ̶̲̅ɪ̶̲̅ɴ̶̲̅s̶̲̅ <andy@a904guy.com>
2017-05-23 06:22:09 -0400

    c long is too large for deque iterator, have to stick to brute force.

--- crunch/__init__.py
+++ crunch/__init__.py
@@ -2,7 +2,16 @@
 # Email: YW5keUBoYXdraW5zLnRlY2g= (base64,utf8)
 # Website: http://hawkins.tech/
 
+from pprint import pformat as pf
 import random
+import itertools
+
+import operator
+from collections import Counter
+from functools import reduce
+from math import factorial
+
+import collections
 
 
 class Crunch:
@@ -10,13 +19,19 @@ class Crunch:
     split_chars = []
     min_length = 1
     max_length = 6
+    started = False
     start_n = 0
     n = 0
 
     def __init__(self, min_length=1, max_length=6, allowed_chars='', start_n=0):
-        for name, x in {'min_length': min_length, 'max_length': max_length, 'start_n': start_n}.items():
+        for name, x in {
+            'min_length': min_length,
+            'max_length': max_length,
+        }.items():
             if isinstance(x, int) is not True or x < 0:
                 raise Exception('Crunch.init: %s=%s must be an integer greater than 0' % (name, x))
+        if (isinstance(start_n, int) is not True and isinstance(start_n, float) is not True) or start_n < 0 is True:
+            raise Exception('Crunch.init: start_n=%s must be an integer or float greater than 0' % pf(start_n))
         if min_length > max_length:
             raise Exception('Crunch.init: Min cannot be less than Max')
         if isinstance(allowed_chars, str) is not True:
@@ -30,11 +45,23 @@ class Crunch:
         self.max_length = max_length
 
     def iterate(self):
-        yield 'ufI53QukIidoxMWeTVww0yJJvrBMrTfLArO5AbrkBIVGtWpn3MlQjDMNP8oKksIsdl42QEASzpY', self.n  # Testing Valid URLS
-        yield 'v4tNS05Q0jWU8A98eeFwbxR55CrnyYT5Nq38MvmkS-qErH1vzXjhnHFIHaRb5r-SH2wc-YNAOI0', self.n  # Testing Valid URLS
+        # yield 'ufI53QukIidoxMWeTVww0yJJvrBMrTfLArO5AbrkBIVGtWpn3MlQjDMNP8oKksIsdl42QEASzpY', self.n  # Testing Valid URLS
+        # yield 'v4tNS05Q0jWU8A98eeFwbxR55CrnyYT5Nq38MvmkS-qErH1vzXjhnHFIHaRb5r-SH2wc-YNAOI0', self.n  # Testing Valid URLS
 
-        while True:
+        for x in itertools.product(self.allowed_chars, repeat=self.max_length):
             self.n += 1
             if self.n < self.start_n:
+                # print("self.n:%s < self.start_n:%s" % (self.n, self.start_n))
                 continue
-            yield ''.join([random.choice(self.split_chars) for x in range(self.max_length-self.min_length, self.max_length)]), self.n
+            yield ''.join(x), self.n
+
+    def brute_force(self):
+        while True:
+            self.n += 1
+            yield ''.join([random.choice(self.split_chars) for x in range(self.max_length - self.min_length, self.max_length)]), self.n
+
+    def n_permutations(self):
+        num = factorial(len(self.split_chars))
+        mults = Counter(self.split_chars).values()
+        den = reduce(operator.mul, (factorial(v) for v in mults), 1)
+        return num / den
--- google_photos.py
+++ google_photos.py
@@ -1,11 +1,10 @@
-import multiprocessing
-import sys
+import multiprocessing, threading
+import math, time
 import subprocess
 from pprint import pprint as pp, pformat
 
 import requests
 
-from Async import Run_Async
 from crunch import Crunch
 
 
@@ -13,22 +12,28 @@ def run_cmd(cmd: str) -> bytes:
     return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=open('/dev/null')).communicate()[0].strip()
 
 
-def brute_force(free_ports):
-    print('Init Crunch')
-    c = Crunch(min_length=75, max_length=75, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-')  # allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-'
+def brute_force(free_ports, start_n):
+    print(f"Start_N: {start_n}")
+    # r = requests.Session()
+    # threads_limit = math.ceil(free_ports / multiprocessing.cpu_count())
+    c = Crunch(min_length=75, max_length=75, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-',
+               start_n=start_n)  # allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-'
     print('Stating Crunch')
-    r = requests.Session()
-    for code, n in c.iterate():
+
+    time.sleep(2)  # Let all the processes spin up
+
+    for code, n in c.brute_force():
         # used_port_count = int(run_cmd("netstat -antp 2> /dev/null | grep 'tcp' | grep -v 'LISTEN' | wc -l"))
 
-        def worker(code, r):
-            print('Testing: http://lh3.googleusercontent.com/%s | n:%s | available_ports:%s\r' % (pformat(code), n * multiprocessing.cpu_count(), free_ports), end='')  #free_ports - used_port_count
+        def worker(code):
+            print('Testing: http://lh3.googleusercontent.com/%s | threading_count: %s | process_name: %s | n:%s\r' % (
+            code, threading.active_count(), multiprocessing.current_process().name.ljust(11), n * multiprocessing.cpu_count()), end='')  # free_ports - used_port_count
             try:
-                resp = r.get('http://lh3.googleusercontent.com/%s' % code, headers={
+                resp = requests.head('http://lh3.googleusercontent.com/%s' % code, headers={
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
                 })
-            except Exception as e:
-                pp(e)
+            except:
+                # pp(e)
                 return
             # print(resp.status_code)
             if resp.status_code == 200:
@@ -36,13 +41,21 @@ def brute_force(free_ports):
                 return
 
         try:
-            Run_Async('threading', 10000).start(worker, (code, r,))  #free_ports - used_port_count
+            while threading.active_count() >= 500:
+                time.sleep(0.1)
+            threading.Thread(target=worker, args=(code,)).start()  # free_ports - used_port_count
         except:
+            # pp(e)
+            # threads_limit = threads_limit - 1
             pass
-    Run_Async('threading').join_all()
 
 
 if __name__ == '__main__':
     max_free_ports = int(run_cmd("sysctl net.ipv4.ip_local_port_range | awk '{print $4-$3}'"))
-    for x in range(1, multiprocessing.cpu_count()):
-        multiprocessing.Process(target=brute_force, args=(max_free_ports,)).start()
+
+    permutations = Crunch(min_length=75, max_length=75, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-').n_permutations()
+    permutations /= multiprocessing.cpu_count()
+    pp("Total Permutations: %s" % permutations)
+    for x in range(0, multiprocessing.cpu_count()):
+        pp("Launching Start_N: %s" % (permutations * x))
+        multiprocessing.Process(target=brute_force, args=(max_free_ports, (x * permutations),)).start()
